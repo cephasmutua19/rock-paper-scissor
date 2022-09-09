b6c2869930efc64307a31aaaaf83cae431d29f71
Cephas Mutua <cephasmutua1984@gmail.com>
2022-09-09 13:10:57 +0300

    Debug game iteration & ending

--- game.js
+++ game.js
@@ -1,8 +1,13 @@
 //Html elements
 let scoreboard = document.querySelectorAll('.score');
 let playersChoice = document.querySelectorAll('.player-button button');
+let playerChoice = document.querySelector('.player-button');
 let compsChoice = document.querySelector('#mark');
 let playButton = document.querySelector('#play-btn');
+let playButtonContainer = document.querySelector('.play-button');
+let p = document.createElement('p');
+p.classList.add('score');
+p.style.fontSize = "3em";
 
 // console.log(scoreboard[0])
 
@@ -17,17 +22,27 @@ function getComputerChoice(){
 };
 
 //Player Buttons
-// playersChoice.forEach(targetZoom)
+playersChoice.forEach(e => e.disabled = true);
 
-function targetZoom(btn){
+function playGame(btn){
+    playButton.addEventListener('click', (e) => {
+        p.textContent = `!!Game On!!`;
+        playButtonContainer.append(p);
+        
+        let targetEl = e.target;
+        targetEl.classList.add('hide');
+        setTimeout(() => {
+            playButtonContainer.removeChild(p);
+            playersChoice.forEach(e => e.disabled = false);
+        }, 2000);
+    });
+
     btn.addEventListener('click', (e) => {
-        e.preventDefault();
         let targetE = e.target;
         let chosenMove =targetE.getAttribute('class');
-        console.log(chosenMove);
         playersChoice.forEach(val => {
             if(val !== targetE){
-                val.classList.add('hide') ;
+                val.classList.add('hide');
             }
             return chosenMove;
         });
@@ -41,75 +56,58 @@ function targetZoom(btn){
 
             let mech = getComputerChoice();
             compsChoice.textContent = '';
-            console.log(mech);
             compsChoice.className = mech;
 
                 setTimeout(() => {
-                    compsChoice.classList.remove(mech)
+                    compsChoice.classList.remove(mech);
                 }, 1300);
 
                 if(mech === chosenMove){
-                    console.log('Draw');
-                }else if(mech === spriteArr[0] && chosenMove === spriteArr[1]){
-                        console.log('you win');
-                        scoreboard[0].innerText = `${++playerCount}`
-                    }else if(mech === spriteArr[0] && chosenMove === spriteArr[2]){
-                            console.log('you loose');
-                            scoreboard[1].innerText = `${++compCount}`
-                        }else if(mech === spriteArr[1] && chosenMove === spriteArr
-                            [0]){
-                                console.log('you loose');
-                                scoreboard[1].innerText = `${++compCount}`
-                            }else if(mech === spriteArr[1] && chosenMove === spriteArr
-                            [2]){
-                                console.log('you win');
-                                scoreboard[0].innerText = `${++playerCount}`
-                            }else if(mech === spriteArr[2] && chosenMove === spriteArr
-                                [0]){
-                                    console.log('you win');
-                                    scoreboard[0].innerText = `${++playerCount}`
-                                }else if(mech === spriteArr[2] && chosenMove === spriteArr
-                                    [1]){
-                                        console.log('you loose');
-                                        scoreboard[1].innerText = `${++compCount}`
-                                    }
+                    scoreboard[0].innerText = `${playerCount += 0}`;
+                    scoreboard[1].innerText = `${compCount += 0}`;
+                }else if(mech === spriteArr[0] && chosenMove === spriteArr[1] || mech === spriteArr[1] && chosenMove === spriteArr
+                    [2] || mech === spriteArr[2] && chosenMove === spriteArr
+                    [0]){
+                        scoreboard[0].innerText = `${++playerCount}`;
+                    }else if(mech === spriteArr[0] && chosenMove === spriteArr[2] || mech === spriteArr[2] && chosenMove === spriteArr[1] || mech === spriteArr[1] && chosenMove === spriteArr
+                        [0]){
+                            scoreboard[1].innerText = `${++compCount}`;
+                        }
+                                    
+                        //End game
+                                    if(scoreboard[0].innerText === '5' || scoreboard[1].innerText === '5'){
+                                        playersChoice.forEach(e => e.disabled = true);
+                                        p.classList.add('score');
+                                        p.style.fontSize = "3em";
+                                        if(compCount > playerCount){
+                                            p.textContent = `You Loose!! Try Again`;
+                                            playButtonContainer.append(p);
+                                        }else if(compCount < playerCount){
+                                            p.textContent = `Awesome!! You Won! Let's do it Again`;
+                                            playButtonContainer.append(p);
+                                        }else{
+                                            p.textContent = `I'ts a Draw!! Let's try Again`;
+                                            playButtonContainer.append(p);
+                                        }
+                                        playerCount = 0;
+                                        compCount = 0;
 
-                                    if((playerCount == 5 || compCount == 5)){
+                                        setTimeout(() => {
+                                            playButtonContainer.removeChild(p);
+                                        }, 4500);
+
                                         setTimeout(() => {
                                             playButton.classList.remove('hide');
                                             scoreboard.forEach(element => {
-                                                element.innerText = '0'
+                                                element.innerText = '0';
                                             });
-                                            playerCount = 0;
-                                        compCount = 0;
-                                        
-    
-                                        }, 3000);
-                                    } 
-
+                                        }, 5000);
+                                    }
     });
-    
+
 };
 
-
-// playersChoice.forEach((e)=>{
-    
-// })
- 
-// Player & Computer choices
-     
-playButton.addEventListener('click', (e) => {
-    // playerCount = 0;
-    // compCount = 0;
-    let targetEl = e.target;
-    targetEl.classList.add('hide');
-    playersChoice.forEach(targetZoom);
-    // console.log(e)
-   
-});
-
-
-
+playersChoice.forEach(playGame);
 
 
 
